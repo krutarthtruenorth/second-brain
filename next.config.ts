2f8768589d78a4279d5bfe4e35ceae2934193eed
gcjunior <gersonczjunior@gmail.com>
2026-05-23 16:25:00 -0600

    Updated external access

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "http://10.245.49.28:3000",
+    "10.245.49.28",
+  ],
 };
 
 export default nextConfig;
